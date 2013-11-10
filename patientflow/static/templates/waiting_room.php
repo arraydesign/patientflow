f026fabd9528bf9286842ce335a5c2c129abf0b8
arraydesign <info@arraydesign.ca>
2013-11-10 12:29:59 -0500

    waiting room view

---
+++ patientflow/static/templates/waiting_room.php
@@ -0,0 +1,104 @@
+<?php $meta_title = 'Waiting Room View';
+$basepage = 'waiting-room';
+$baseclass = 'waiting-room-view';
+include('header.php'); ?>
+
+<!-- - - - - - - - - - CONTENT STARTS - - - - - - - - - -->
+
+<div class="content">
+
+	<ul class="genbox col-full inline patients">
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">301</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">- - -</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">- - -</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">- - -</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">- - -</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">- - -</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">- - -</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">- - -</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">- - -</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+		<li class="patient" id="patient-id-0000000">
+			<span class="name">Firstname Lastname</span>
+			<span class="practitioner-name">Dr. Keith Chung</span>
+			<span class="room-number">- - -</span>
+			<span class="appt-start">11:30am</span>
+			<span class="arrived">(11:25am)</span>
+			<span class="appt-length">11 / 20</span>
+			<span class="waiting-time">04 / 05</span>
+		</li>
+	</ul>
+
+</div><!-- END .content -->
+<?php include('footer.php'); ?>
